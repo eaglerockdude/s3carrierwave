5b48fad692bc7e2f329f7efb30d737d9cc36a885
ken mcfadden <kenmcfadden@kmac-2.local>
2015-03-01 09:03:39 -0800

    final but before s3

---
+++ picturesque/app/controllers/galleries_controller.rb
@@ -0,0 +1,53 @@
+class GalleriesController < ApplicationController
+
+
+  def index
+    @galleries = Gallery.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    # @gallery = Gallery.new(params[:gallery])
+    @gallery=Gallery.new(gallery_params)
+    if @gallery.save
+      flash[:notice] = "Successfully created gallery."
+      redirect_to @gallery
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:id])
+    if @gallery.update(gallery_parms)
+      flash[:notice] = "Successfully updated gallery."
+      redirect_to gallery_url
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:id])
+    @gallery.destroy
+    flash[:notice] = "Successfully destroyed gallery."
+    redirect_to galleries_url
+  end
+
+  private
+    def gallery_params
+      params.require(:gallery).permit!
+    end
+
+end
